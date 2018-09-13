922ec40a976ad7f1c2f363c33368f0c4973cc6aa
Tianyang Liu <anyyhmy@gmail.com>
2018-09-13 16:25:04 +0800

    migration
    create migration for client, record, and expense

--- Backend-Leitzu/database/migrations/2018_09_13_073435_create_expenses_table.php
+++ Backend-Leitzu/database/migrations/2018_09_13_073435_create_expenses_table.php
@@ -13,8 +13,16 @@ class CreateExpensesTable extends Migration
      */
     public function up()
     {
+        /*
+         * activity_cost 
+         * 会记录做的项目和价格
+         * 可能由多个项目组合
+         * 格式大概为： 剪发&30/烫染&50
+         * 上面例子是两个项目，价格用'&'号区分，项目区分用'/'
+         */
         Schema::create('expenses', function (Blueprint $table) {
             $table->increments('id');
+            $table->text('activity_cost');
             $table->timestamps();
         });
     }
