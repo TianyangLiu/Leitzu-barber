a2e593e2a571a647717ea5918d4b4a83f804447c
Tianyang Liu <anyyhmy@gmail.com>
2018-09-15 22:03:02 +0800

    create basic components
    added navber, client, record, and expense components

--- Frontend-Leitzu/src/app/app.module.ts
+++ Frontend-Leitzu/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ClientsComponent } from './components/clients/clients.component';
+import { RecordsComponent } from './components/records/records.component';
+import { ExpensesComponent } from './components/expenses/expenses.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ClientsComponent,
+    RecordsComponent,
+    ExpensesComponent
   ],
   imports: [
     BrowserModule
