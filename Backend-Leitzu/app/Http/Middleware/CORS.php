81c10b0820cf64c3bd008e0745582f36b3e56a85
Tianyang Liu <anyyhmy@gmail.com>
2018-09-16 00:51:39 +0800

    cors

---
+++ Backend-Leitzu/app/Http/Middleware/CORS.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class CORS
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        Header('Access-Control-Allow-Origin : *');
+
+        Header('Access-Control-Allow-Methods : *');
+
+        Header('Access-Control-Allow-headers : Content-type, X-Auth-Token, Authorization, Origin');
+        
+        return $next($request);
+    }
+}
--- Backend-Leitzu/routes/api.php
+++ Backend-Leitzu/routes/api.php
@@ -16,3 +16,17 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group([
+
+    'middleware' => 'api',
+    
+], function ($router) {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('sign-up', 'AuthController@signup');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
+});
